c3f55bed60dc8f31d6beadcade9d7db72a8b98cf
Susanna Klosko <sdklos@gmail.com>
2017-07-03 18:07:41 +0000

    writing scrape method

--- lib/world_heritage_sites.rb
+++ lib/world_heritage_sites.rb
@@ -1,2 +1,39 @@
+require 'open-uri'
+require 'pry'
+require 'nokogiri'
+
 class WorldHeritageSites
+  attr_accessor :name
+  @@all = []
+
+  def initialize(name)
+    @name = name
+  end
+
+  def self.all
+    @@all
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.scrape_countries
+    doc = Nokogiri::HTML(open('http://whc.unesco.org/en/list/'))
+    box = doc.css("div#acc.box")
+    countries = []
+    box.css("h4 a").text.each do |country|
+      countries << country
+    end
+    binding.pry
+    instance.save
+    instance
+  end
+
+  def self.new_from_url(url)
+
+  end
+
 end
+
+WorldHeritageSites.scrape_countries
